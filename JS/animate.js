4be8daaefbde7778697e1fe13dd4cfb2a61e6fcf
Yami <yami.cardona@WGInc.com>
2017-08-01 16:01:29 -0400

    Autplay slider

--- JS/animate.js
+++ JS/animate.js
@@ -7,7 +7,7 @@ jq(document).ready(function () {
 
     owl.owlCarousel({
         pagination: false,
-        autoPlay: 3500,
+        autoPlay: 2500,
         stopOnHover: true,
         items: 10, //10 items above 1000px browser width
         itemsDesktop: [1199, 6], //6 items between 1000px and 901px
