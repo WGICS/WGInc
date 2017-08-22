9573a5458e43af8bbc937546110bc00779d77460
Yami <yami.cardona@WGInc.com>
2017-08-22 15:36:29 -0400

    meet our team - removed next and previous

--- JS/animate.js
+++ JS/animate.js
@@ -14,7 +14,7 @@ jq(document).ready(function () {
 		itemsDesktopSmall: [900, 4], // betweem 900px and 601px
 		itemsMobile: [600, 3], //2 items between 600 and 0
 		itemsTablet: [600, 3], //2 items between 600 and 0
-        navigation: true;
+       
     });
 
     var owl = jq("#owl-hello");
@@ -28,6 +28,6 @@ jq(document).ready(function () {
 		itemsDesktopSmall: [900, 4], // betweem 900px and 601px
 		itemsMobile:  [ 600, 3], //3 items between 600 and 0
 		itemsTablet: [600, 3], //3 items between 600 and 0
-        navigation: true;
+        
     });
 });
