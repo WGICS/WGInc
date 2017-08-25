0e5f5ffcede554ea4231e6bc90ed34a4489d841e
Javier Olaya <jolaya@fau.edu>
2017-08-25 10:09:28 -0400

    Grayscale

--- functions.php
+++ functions.php
@@ -12,6 +12,15 @@
 
 	add_action( 'wp_enqueue_scripts', 'owl_scripts' );
 
+	function gray_scripts() {
+
+		wp_register_script('gray', get_stylesheet_directory_uri(). '/JS/jquery.gray.min.js', array('jquery'),'1.1', true);
+
+		wp_enqueue_script('gray');
+	}
+
+	add_action( 'wp_enqueue_scripts', 'gray_scripts' );
+
 	function wpb_adding_scripts() {
 
 		wp_register_script('my_animate', get_stylesheet_directory_uri(). '/JS/animate.js', array('jquery'),'1.1', true);
