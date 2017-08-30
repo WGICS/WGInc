879d45fe94a5866a34b3f3ff05a0382ef169227a
Javier Olaya <jolaya@fau.edu>
2017-08-30 14:16:30 -0400

    Add Divi to all post types

--- functions.php
+++ functions.php
@@ -30,5 +30,41 @@
 
 	add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
 
-
+    // Divi Builder on custom post types by https://wpcolt.com
+    add_filter('et_builder_post_types', 'divicolt_post_types');
+    add_filter('et_fb_post_types','divicolt_post_types' ); // Enable Divi Visual Builder on the custom post types
+    function divicolt_post_types($post_types)
+    {
+        foreach (get_post_types() as $post_type) {
+            if (!in_array($post_type, $post_types) and post_type_supports($post_type, 'editor')) {
+                $post_types[] = $post_type;
+            }
+        }
+        return $post_types;
+    }
+    add_action('add_meta_boxes', 'divicolt_add_meta_box');
+    function divicolt_add_meta_box()
+    {
+        foreach (get_post_types() as $post_type) {
+            if (post_type_supports($post_type, 'editor') and function_exists('et_single_settings_meta_box')) {
+                $obj= get_post_type_object( $post_type );
+                add_meta_box('et_settings_meta_box', sprintf(__('Divi %s Settings', 'Divi'), $obj->labels->singular_name), 'et_single_settings_meta_box', $post_type, 'side', 'high');
+            }
+        }
+    }
+    add_action('admin_head', 'divicolt_admin_js');
+    function divicolt_admin_js()
+    {
+        $s = get_current_screen();
+        if (!empty($s->post_type) and $s->post_type != 'page' and $s->post_type != 'post') {
+            ?>
+        <script>
+            jQuery(function ($) {
+                $('#et_pb_layout').insertAfter($('#et_pb_main_editor_wrap'));
+            });
+        </script>
+        <?php
+        }
+    }
+    //
 ?>
